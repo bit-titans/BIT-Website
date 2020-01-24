48ad90453d0bc000fa00f704d1867877fc25eab0
rottiakash <rottiakash@gmail.com>
2020-01-24 13:45:07 +0530

    Added Docker

--- app.py
+++ app.py
@@ -1,32 +1,9 @@
 from flask import Flask,render_template
-import requests
-from lxml import html
 import math
 app = Flask(__name__)
-app.config.from_pyfile('config.cfg')
 import mysql.connector
 @app.route('/')
 def index():
-   # TODO: Change to AJAX
-   #Facebook Likes Count
-   page = requests.get("https://www.facebook.com/bitsince1979/")
-   tree = html.fromstring(page.content)
-   fcount = tree.xpath('//*[@id="PagesProfileHomeSecondaryColumnPagelet"]/div/div[2]/div/div[1]/div[2]/div/div[2]/div/text()')
-   fcount = fcount[0][0:3]
-   #Twitter Follower Count
-   url = "https://api.twitter.com/1.1/users/show.json?screen_name=bitsince1979"
-   payload = {}
-   OAUTH_CONSUMER_KEY = app.config['OAUTH_CONSUMER_KEY']
-   OAUTH_TOKEN = app.config['OAUTH_TOKEN']
-   OAUTH_TIMESTAMP = app.config['OAUTH_TIMESTAMP']
-   OAUTH_NONCE = app.config['OAUTH_NONCE']
-   OAUTH_SIGNATURE = app.config['OAUTH_SIGNATURE']
-   oauth = 'OAuth oauth_consumer_key="%s",oauth_token="%s",oauth_signature_method="HMAC-SHA1",oauth_timestamp="%s",oauth_nonce="%s",oauth_version="1.0",oauth_signature="%s"' %(OAUTH_CONSUMER_KEY,OAUTH_TOKEN,OAUTH_TIMESTAMP,OAUTH_NONCE,OAUTH_SIGNATURE)
-   headers = {
-   'Authorization': oauth
-   }
-
-   tcount = requests.request("GET", url, headers=headers, data = payload)
    mydb = mysql.connector.connect(
   host="ec2-15-206-77-23.ap-south-1.compute.amazonaws.com",
   user="root",
@@ -35,35 +12,14 @@ auth_plugin='mysql_native_password'
 )
    mycursor = mydb.cursor()
    mycursor.execute("use bit")
-   tcount = tcount.json()['followers_count']
    mycursor = mydb.cursor()
    mycursor.execute("SELECT * FROM Announcements order by id desc LIMIT 5")
    myresult = mycursor.fetchall()
-   return render_template('index.html', fcount=fcount, tcount=tcount, annons=myresult,getDate=getDate)
+   return render_template('index.html', annons=myresult,getDate=getDate)
 
 @app.route('/about')
 def about():
-    #Facebook Likes Count
-   page = requests.get("https://www.facebook.com/bitsince1979/")
-   tree = html.fromstring(page.content)
-   fcount = tree.xpath('//*[@id="PagesProfileHomeSecondaryColumnPagelet"]/div/div[2]/div/div[1]/div[2]/div/div[2]/div/text()')
-   fcount = fcount[0][0:3]
-   #Twitter Follower Count
-   url = "https://api.twitter.com/1.1/users/show.json?screen_name=bitsince1979"
-   payload = {}
-   OAUTH_CONSUMER_KEY = app.config['OAUTH_CONSUMER_KEY']
-   OAUTH_TOKEN = app.config['OAUTH_TOKEN']
-   OAUTH_TIMESTAMP = app.config['OAUTH_TIMESTAMP']
-   OAUTH_NONCE = app.config['OAUTH_NONCE']
-   OAUTH_SIGNATURE = app.config['OAUTH_SIGNATURE']
-   oauth = 'OAuth oauth_consumer_key="%s",oauth_token="%s",oauth_signature_method="HMAC-SHA1",oauth_timestamp="%s",oauth_nonce="%s",oauth_version="1.0",oauth_signature="%s"' %(OAUTH_CONSUMER_KEY,OAUTH_TOKEN,OAUTH_TIMESTAMP,OAUTH_NONCE,OAUTH_SIGNATURE)
-   headers = {
-   'Authorization': oauth
-   }
-
-   tcount = requests.request("GET", url, headers=headers, data = payload)
-   tcount = tcount.json()['followers_count']
-   return render_template('about.html',fcount=fcount, tcount=tcount)
+   return render_template('about.html')
 
 @app.route('/placement')
 def placement():
